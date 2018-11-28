9db9629b03fecc479d2cacb9578b946deba97f46
Manomano Takudzwa Danmore <takudzwama@oldmutual.co.zw>
2018-11-28 02:25:30 +0200

    added a staff category

--- PeopleFit.Data/Models/Position.cs
+++ PeopleFit.Data/Models/Position.cs
@@ -7,12 +7,18 @@ namespace PeopleFit.Data.Models
     {
         I,J,K,L,M,N,O,P,R,S,T,U
     }
+
+    public enum StaffCategory
+    {
+        Staff,Sales,Specialist,Management
+    }
     public class Position
     {
         public int ID {get; set;}
         public string Title { get; set; }
         public int ThemeOfWork { get; set; }
         public RoleSize RoleSize { get; set; }
+        public StaffCategory StaffCategory { get; set; }
         public string Description { get; set; }
         public string Requirements { get; set; }
         public string BusinessUnit { get; set; }
