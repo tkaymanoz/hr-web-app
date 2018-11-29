fdabdca963cbf3a5d449bae8f7cc11c8d05875d1
Manomano Takudzwa Danmore <takudzwama@oldmutual.co.zw>
2018-11-29 08:23:21 +0200

    added a department object

---
+++ PeopleFit.Data/Models/Department.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace PeopleFit.Data.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
+
+        public ICollection<Position> Positions { get; set; }
+    }
+}
